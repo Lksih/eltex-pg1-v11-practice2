798a7e36e90f37519effae06e3aa8ce8a2c1924f
Lksih <odindwatri123123@gmail.com>
2026-03-18 18:43:22 +0700

    delete

--- module2/phonebook.c
+++ module2/phonebook.c
@@ -1,14 +1,14 @@
 #include "phonebook.h"
 
-int init_phonebook(phonebook pb)
+int init_phonebook(phonebook *pb)
 {
     int res = 0;
 
-    pb.contacts = malloc(sizeof(contact) * PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP);
-    if (pb.contacts)
+    pb->contacts = malloc(sizeof(contact) * PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP);
+    if (pb->contacts)
     {
-        pb.contacts_capacity = PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
-        pb.contacts_quan = 0;
+        pb->contacts_capacity = PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
+        pb->contacts_quan = 0;
     }
     else
     {
@@ -18,17 +18,25 @@ int init_phonebook(phonebook pb)
     return res;
 }
 
-int add_contact(phonebook pb, contact new_contact)
+void delete_phonebook(phonebook *pb)
+{
+    free(pb->contacts);
+    pb->contacts = 0;
+    pb->contacts_capacity = 0;
+    pb->contacts_quan = 0;
+}
+
+int add_contact_to_phonebook(phonebook *pb, contact new_contact)
 {
     int res = 0;
 
-    if (pb.contacts_quan == pb.contacts_capacity)
+    if (pb->contacts_quan == pb->contacts_capacity)
     {
-        contact *new_contacts = realloc(pb.contacts, sizeof(contact) * (pb.contacts_capacity + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP));
+        contact *new_contacts = realloc(pb->contacts, sizeof(contact) * (pb->contacts_capacity + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP));
         if (new_contacts)
         {
-            pb.contacts = new_contacts;
-            pb.contacts_capacity += PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
+            pb->contacts = new_contacts;
+            pb->contacts_capacity += PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
         }
         else
         {
@@ -38,8 +46,39 @@ int add_contact(phonebook pb, contact new_contact)
 
     if (!res)
     {
-        pb.contacts[pb.contacts_quan] = new_contact;
-        pb.contacts_quan++;
+        pb->contacts[pb->contacts_quan] = new_contact;
+        pb->contacts_quan++;
+    }
+
+    return res;
+}
+
+int delete_contact_from_phonebook(phonebook *pb, unsigned int ind)
+{
+    int res = 0;
+
+    if (ind >= pb->contacts_quan)
+    {
+        res = 1;
+    }
+    else
+    {
+        delete_contact(&(pb->contacts[ind]));
+        for (unsigned int i = ind + 1; i < pb->contacts_quan; i++)
+        {
+            pb->contacts[i - 1] = pb->contacts[i];
+        }
+        pb->contacts_quan--;
+    }
+
+    if (pb->contacts_capacity > pb->contacts_quan + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP)
+    {
+        contact *new_contacts = realloc(pb->contacts, sizeof(contact) * (pb->contacts_quan + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP));
+        if (new_contacts)
+        {
+            pb->contacts = new_contacts;
+            pb->contacts_capacity = pb->contacts_quan + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
+        }
     }
 
     return res;
