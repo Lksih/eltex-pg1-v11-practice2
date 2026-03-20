89dbe3683ea7456ee2d8087151ad30d85ef5bf39
Lksih <odindwatri123123@gmail.com>
2026-03-20 15:09:59 +0700

    правки contact

--- module2/2.1/contact.c
+++ module2/2.1/contact.c
@@ -5,16 +5,72 @@ int init_contact(contact *c)
     int res = 0;
 
     init_names(&(c->names));
+    init_address(&(c->address));
 
-    c->social_media_links = malloc(sizeof(contact) * SOCIAL_MEDIA_LINKS_CAPACITY_INCREASE_STEP);
+    c->social_media_links = malloc(sizeof(char *) * SOCIAL_MEDIA_LINKS_CAPACITY_INCREASE_STEP);
     if (c->social_media_links)
     {
-        c->social_media_links = SOCIAL_MEDIA_LINKS_CAPACITY_INCREASE_STEP;
-        c->social_media_links = 0;
+        c->social_media_links_capacity = SOCIAL_MEDIA_LINKS_CAPACITY_INCREASE_STEP;
+        c->social_media_links_quan = 0;
     }
     else
     {
         res = 1;
+    }
+
+    if (res == 0)
+    {
+        c->emails = malloc(sizeof(char *) * EMAILS_CAPACITY_INCREASE_STEP);
+        if (c->emails)
+        {
+            c->emails_capacity = EMAILS_CAPACITY_INCREASE_STEP;
+            c->emails_quan = 0;
+        }
+        else
+        {
+            res = 1;
+        }
+    }
+
+    if (res == 0)
+    {
+        c->phone_numbers = malloc(sizeof(char *) * PHONE_NUMBERS_CAPACITY_INCREASE_STEP);
+        if (c->phone_numbers)
+        {
+            c->phone_numbers_capacity = PHONE_NUMBERS_CAPACITY_INCREASE_STEP;
+            c->phone_numbers_quan = 0;
+        }
+        else
+        {
+            res = 1;
+        }
+    }
+
+    return res;
+}
+
+int add_social_media_link_to_contact(contact *c, contact new_link)
+{
+    int res = 0;
+
+    if (c->social_media_links_quan == c->social_media_links_capacity)
+    {
+        char **new_contacts = realloc(c->contacts, sizeof(contact) * (c->contacts_capacity + PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP));
+        if (new_contacts)
+        {
+            c->contacts = new_contacts;
+            c->contacts_capacity += PHONEBOOK_CONTACTS_CAPACITY_INCREASE_STEP;
+        }
+        else
+        {
+            res = 1;
+        }
+    }
+
+    if (!res)
+    {
+        c->contacts[c->contacts_quan] = new_link;
+        c->contacts_quan++;
     }
 
     return res;
@@ -22,8 +78,23 @@ int init_contact(contact *c)
 
 void delete_contact(contact *c)
 {
-    free(c->social_media_links);
+    free_strings(c->social_media_links, c->social_media_links_capacity);
+    c->social_media_links = 0;
+
+    free_strings(c->emails, c->emails_capacity);
+    c->emails = 0;
+
+    free_strings(c->phone_numbers, c->phone_numbers_capacity);
+    c->phone_numbers = 0;
+
     c->social_media_links = 0;
     c->social_media_links_capacity = 0;
     c->social_media_links_quan = 0;
+}
+
+void free_strings(char **strings, unsigned int quan){
+    for (unsigned int i=0;i<quan;i++){
+        free(strings[i]);
+    }
+    free(strings);
 }
--- module2/2.1/contact.h
+++ module2/2.1/contact.h
@@ -5,17 +5,31 @@
 #include "address.h"
 
 const unsigned int SOCIAL_MEDIA_LINKS_CAPACITY_INCREASE_STEP = 10;
+const unsigned int EMAILS_CAPACITY_INCREASE_STEP = 10;
+const unsigned int PHONE_NUMBERS_CAPACITY_INCREASE_STEP = 10;
+
+const unsigned int SOCIAL_MEDIA_LINK_SIZE = 1001;
+const unsigned int EMAIL_SIZE = 256;
+const unsigned int PHONE_NUMBER_SIZE = 11;
 
 typedef struct {
     names names;
     address address;
-    char email[256];
-    char phone_number[11];
+    char workplace[101];
+    char position[51];
     char flat[20];
 
     unsigned char social_media_links_quan;
     unsigned char social_media_links_capacity;
     char** social_media_links;
+
+    unsigned char emails_quan;
+    unsigned char emails_capacity;
+    char** emails;
+
+    unsigned char phone_numbers_quan;
+    unsigned char phone_numbers_capacity;
+    char** phone_numbers;
 } contact;
 
 #endif
