3d18e404bca12798ceae2c3d84c8f769ef549b47
Lksih <ilya.vorobev09@gmail.com>
2026-05-06 15:02:15 +0700

    TCP доделка

--- module3/13/tcp_client.c
+++ module3/13/tcp_client.c
@@ -5,109 +5,212 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+
+#define STR_SIZE 1024
+#define NAME_SIZE 256
+#define FILE_SIZE 4096
 
 void error(const char *msg)
 {
     perror(msg);
-    exit(0);
+    exit(EXIT_FAILURE);
 }
+
+ssize_t read_line(int sock, char *buf, size_t maxlen);
+ssize_t recv_all(int sock, void *buf, size_t len);
 
 int main(int argc, char *argv[])
 {
     int my_sock, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    char send_buf[STR_SIZE];
+    char recv_buf[STR_SIZE];
 
-    //char buff[256];
-	char buff[1024];
     printf("TCP DEMO CLIENT\n");
-	
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
     // извлечение порта
-	portno = atoi(argv[2]);
-    
-	// Шаг 1 - создание сокета
-	my_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (my_sock < 0) 
+    portno = atoi(argv[2]);
+
+    // Шаг 1 - создание сокета
+    my_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (my_sock < 0)
         error("ERROR opening socket");
     // извлечение хоста
-	server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(EXIT_FAILURE);
     }
     // заполенние структуры serv_addr
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
     // установка порта
-	serv_addr.sin_port = htons(portno);
-    
-	// Шаг 2 - установка соединения	
-	if (connect(my_sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    serv_addr.sin_port = htons(portno);
+
+    // Шаг 2 - установка соединения
+    if (connect(my_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    
-	// Шаг 3 - чтение и передача сообщений
-	while ((n = recv(my_sock, &buff[0], sizeof(buff) - 1, 0)) > 0)
-     {
-         // ставим завершающий ноль в конце строки
-         buff[n] = 0;
 
-         // выводим на экран
-         printf("S=>C:%s", buff);
+    // Шаг 3 - чтение и передача сообщений
+    if (read_line(my_sock, recv_buf, sizeof(recv_buf)) > 0)
+    {
+        printf("Server: %s\n", recv_buf);
+    }
 
-         // читаем пользовательский ввод с клавиатуры
-         printf("S<=C:"); 
-		 fgets(&buff[0], sizeof(buff) - 1, stdin);
+    while (1)
+    {
+        printf("S<=C: ");
+        fflush(stdout);
+        if (fgets(send_buf, sizeof(send_buf), stdin) == NULL)
+        {
+            break;
+        }
 
-         // проверка на "quit"
-         if (!strcmp(&buff[0], "quit\n"))
-         {
-             // Корректный выход
-             printf("Exit...");
-             close(my_sock);
-              return 0;
-         }
+        size_t len = strlen(send_buf);
+        if (len > 0 && send_buf[len - 1] == '\n')
+        {
+            send_buf[len - 1] = '\0';
+        }
 
-         // передаем строку клиента серверу
-         send(my_sock, &buff[0], strlen(&buff[0]), 0);
-     }
-     printf("Recv error \n");
-     close(my_sock);
-     return -1;
-	
-	/*
-	printf("Please enter the message: ");
-    bzero(buff,256);
-    fgets(buff,255,stdin);
-    n = write(sockfd,buff,strlen(buff));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    
-	bzero(buff,256);
-    n = read(sockfd,buff,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buff);
-    
-	printf("Please enter the message: ");
-    bzero(buff,256);
-    fgets(buff,255,stdin);
-    n = write(sockfd,buff,strlen(buff));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    
-	bzero(buff,256);
-    n = read(sockfd,buff,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buff);
-	close(sockfd);
-    return 0;*/
+        char packet[STR_SIZE];
+        snprintf(packet, sizeof(packet), "%s\n", send_buf);
+        if (send(my_sock, packet, strlen(packet), 0) < 0)
+        {
+            perror("send");
+            break;
+        }
+
+        // проверка на "quit"
+        if (!strcmp(send_buf, "quit"))
+        {
+            break;
+        }
+
+        if (strncmp(send_buf, "GET ", 4) == 0)
+        {
+            char filename[NAME_SIZE];
+            if (sscanf(send_buf, "GET %255s", filename) != 1)
+            {
+                printf("usage: GET filename\n");
+                continue;
+            }
+            
+            if (read_line(my_sock, recv_buf, sizeof(recv_buf)) <= 0)
+            {
+                printf("Connection closed\n");
+                break;
+            }
+            if (strncmp(recv_buf, "OK ", 3) == 0)
+            {
+                long size = atol(recv_buf + 3);
+                printf("Receiving file '%s' (%ld bytes)\n", filename, size);
+                FILE *fp = fopen(filename, "wb");
+                if (!fp)
+                {
+                    perror("fopen");
+                    char dummy[FILE_SIZE];
+                    long remain = size;
+                    while (remain > 0)
+                    {
+                        n = recv(my_sock, dummy, sizeof(dummy) < remain ? sizeof(dummy) : remain, 0);
+                        if (n <= 0)
+                        {
+                            break;
+                        }
+                        remain -= n;
+                    }
+                    continue;
+                }
+                char buf[FILE_SIZE];
+                long remain = size;
+                while (remain > 0)
+                {
+                    n = recv(my_sock, buf, sizeof(buf) < remain ? sizeof(buf) : remain, 0);
+                    if (n <= 0)
+                    {
+                        break;
+                    }
+                    fwrite(buf, 1, n, fp);
+                    remain -= n;
+                }
+                fclose(fp);
+                if (remain == 0)
+                {
+                    printf("File '%s' saved.\n", filename);
+                }
+                else
+                {
+                    printf("Error receiving file.\n");
+                }
+            }
+            else
+            {
+                printf("%s\n", recv_buf);
+            }
+        }
+        else
+        {
+            if (read_line(my_sock, recv_buf, sizeof(recv_buf)) > 0)
+            {
+                printf("S=>C: %s\n", recv_buf);
+            }
+            else
+            {
+                printf("Connection closed by server.\n");
+                break;
+            }
+        }
+    }
+
+    close(my_sock);
+    return 0;
+}
+
+ssize_t read_line(int sock, char *buf, size_t maxlen)
+{
+    size_t i = 0;
+    ssize_t n;
+    char c;
+    while (i < maxlen - 1)
+    {
+        n = recv(sock, &c, 1, 0);
+        if (n <= 0)
+        {
+            return n;
+        }
+        if (c == '\n')
+        {
+            break;
+        }
+        buf[i++] = c;
+    }
+    buf[i] = '\0';
+    return i;
+}
+
+ssize_t recv_all(int sock, void *buf, size_t len)
+{
+    size_t total = 0;
+    char *ptr = (char *)buf;
+    while (total < len)
+    {
+        ssize_t n = recv(sock, ptr + total, len - total, 0);
+        if (n <= 0)
+        {
+            return n;
+        }
+        total += n;
+    }
+    return total;
 }
--- module3/13/tcp_server.c
+++ module3/13/tcp_server.c
@@ -1,15 +1,17 @@
-/* Пример простого TCP сервера
-   Порт является аргументом, для запуска сервера неободимо ввести:
-   # ./[имя_скомпилированного_файла] [номер порта]
-   # ./server 57123
-*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define STR_SIZE 1024
+#define NAME_SIZE 256
+#define FILE_SIZE 4096
 
 // прототип функции, обслуживающей
 // подключившихся пользователей
@@ -19,136 +21,244 @@ void dostuff(int);
 void error(const char *msg)
 {
     perror(msg);
-    exit(1);
+    exit(EXIT_FAILURE);
 }
 
 // глобальная переменная – количество
-// активных пользователей 
+// активных пользователей
 int nclients = 0;
 
 // макрос для печати количества активных
-// пользователей 
+// пользователей
 void printusers()
-{ 
-	if(nclients)
-	{printf("%d user on-line\n",nclients);}
-	else {printf("No User on line\n");}
-}
-
-// функция по варианту
-int myfunc(int a, int b)
 {
-	return a+b;
+    if (nclients)
+    {
+        printf("%d user on-line\n", nclients);
+    }
+    else
+    {
+        printf("No User on line\n");
+    }
 }
 
-
+ssize_t read_line(int sock, char *buf, size_t maxlen);
+ssize_t send_all(int sock, const void *buf, size_t len);
 
 int main(int argc, char *argv[])
 {
-    char buff[1024];    // Буфер для различных нужд
-	printf("TCP SERVER DEMO\n");
-	
-	int sockfd, newsockfd; // дескрипторы сокетов
-	int portno; // номер порта
-	int pid; // id номер потока
-    socklen_t clilen; // размер адреса клиента типа socklen_t
+    printf("TCP SERVER DEMO\n");
+
+    int sockfd, newsockfd;                  // дескрипторы сокетов
+    int portno;                             // номер порта
+    int pid;                                // id номер потока
+    socklen_t clilen;                       // размер адреса клиента типа socklen_t
     struct sockaddr_in serv_addr, cli_addr; // структура сокета сервера и клиента
-	
-	// ошибка в случае если мы не указали порт
-    if (argc < 2) 
-	{
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
+
+    // ошибка в случае если мы не указали порт
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(EXIT_FAILURE);
     }
     // Шаг 1 - создание сокета
-	// AF_INET     - сокет Интернета
+    // AF_INET     - сокет Интернета
     // SOCK_STREAM  - потоковый сокет (с
     //      установкой соединения)
     // 0 - по умолчанию выбирается TCP протокол
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     // ошибка при создании сокета
-	if (sockfd < 0) 
-       error("ERROR opening socket");
-	 
-	// Шаг 2 - связывание сокета с локальным адресом
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    // Шаг 2 - связывание сокета с локальным адресом
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY; // сервер принимает подключения на все IP-адреса
     serv_addr.sin_port = htons(portno);
     // вызываем bind для связывания
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
     // Шаг 3 - ожидание подключений, размер очереди - 5
-	listen(sockfd,5);
+    listen(sockfd, 5);
     clilen = sizeof(cli_addr);
-	
-	// Шаг 4 - извлекаем сообщение из очереди
-	// цикл извлечения запросов на подключение из очереди    
-	while (1) 
-	{
-         newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
-             error("ERROR on accept");
-	nclients++; // увеличиваем счетчик
-              // подключившихся клиентов
-		/*
-		// вывод сведений о клиенте
-		struct hostent *hst;
-		hst = gethostbyaddr((char *)&cli_addr.sin_addr, 4, AF_INET);
-		printf("+%s [%s] new connect!\n",
-		(hst) ? hst->h_name : "Unknown host",
-		(char*)inet_ntoa(cli_addr.sin_addr));*/
-	printusers();
-		
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  
-		 {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
-     } /* end of while */
-     close(sockfd);
-     return 0; /* we never get here */
+
+    // Шаг 4 - извлекаем сообщение из очереди
+    // цикл извлечения запросов на подключение из очереди
+    while (1)
+    {
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+        {
+            perror("ERROR on accept");
+            continue;
+        }
+        nclients++; // увеличиваем счетчик
+                    // подключившихся клиентов
+                    /*
+                    // вывод сведений о клиенте
+                    struct hostent *hst;
+                    hst = gethostbyaddr((char *)&cli_addr.sin_addr, 4, AF_INET);
+                    printf("+%s [%s] new connect!\n",
+                    (hst) ? hst->h_name : "Unknown host",
+                    (char*)inet_ntoa(cli_addr.sin_addr));*/
+        printusers();
+
+        pid = fork();
+        if (pid < 0)
+            error("ERROR on fork");
+        if (pid == 0)
+        {
+            close(sockfd);
+            dostuff(newsockfd);
+            exit(0);
+        }
+        else
+            close(newsockfd);
+    } /* end of while */
+    close(sockfd);
+    return 0; /* we never get here */
 }
 
-void dostuff (int sock)
+void dostuff(int sock)
 {
-	//int n,a,b;
-	int bytes_recv; // размер принятого сообщения
-	int a,b; // переменные для myfunc
-	char buff[20 * 1024];
-	#define str1 "Enter 1 parametr\r\n"
-	#define str2 "Enter 2 parametr\r\n" 
-    	
-	// отправляем клиенту сообщение
-   	 write(sock, str1, sizeof(str1));
-   
-   	// обработка первого параметра
-	bytes_recv = read(sock,&buff[0],sizeof(buff));
-	if (bytes_recv < 0) error("ERROR reading from socket");
-	a = atoi(buff); // преобразование первого параметра в int
-	
-	// отправляем клиенту сообщение
-	write(sock,str2,sizeof(str2));
-	
-	bytes_recv = read(sock,&buff[0],sizeof(buff));
-	if (bytes_recv < 0) error("ERROR reading from socket");
-	
-	b = atoi(buff); // преобразование второго параметра в int
-	a = myfunc(a,b); // вызов пользовательской функции
-	snprintf(buff, strlen(buff), "%d", a); // преобразование результата выполнения в строку
-	buff[strlen(buff)] = '\n'; // добавление к сообщению символа конца строки
-	
-	// отправляем клиенту результат
-	write(sock,&buff[0],sizeof(buff));
-	
-	nclients--; // уменьшаем счетчик активных клиентов
-    	printf("-disconnect\n"); 
-	printusers();
-	return;
+    char line[STR_SIZE];
+    char response[STR_SIZE];
+    char operation;
+    double a, b, result;
+
+    const char *welcome = "Commands: MATH +|-|*|/ num1 num2, GET filename, quit\n";
+    send_all(sock, welcome, strlen(welcome));
+
+    while (1)
+    {
+        ssize_t len = read_line(sock, line, sizeof(line));
+        if (len <= 0)
+        {
+            break;
+        }
+
+        if (strncmp(line, "quit", 4) == 0)
+        {
+            break;
+        }
+        else if (strncmp(line, "MATH ", 5) == 0)
+        {
+            if (sscanf(line, "MATH %c %lf %lf", &operation, &a, &b) != 3)
+            {
+                send_all(sock, "ERROR Invalid MATH format\n", 26);
+                continue;
+            }
+            switch (operation)
+            {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0.0)
+                {
+                    send_all(sock, "ERROR Division by zero\n", 23);
+                    continue;
+                }
+                result = a / b;
+                break;
+            default:
+                send_all(sock, "ERROR Unknown operation\n", 24);
+                continue;
+            }
+            snprintf(response, sizeof(response), "Result: %.6g\n", result);
+            send_all(sock, response, strlen(response));
+        }
+        else if (strncmp(line, "GET ", 4) == 0)
+        {
+            char filename[NAME_SIZE];
+            if (sscanf(line, "GET %255s", filename) != 1)
+            {
+                send_all(sock, "ERROR No filename\n", 18);
+                continue;
+            }
+            int fd = open(filename, O_RDONLY);
+            if (fd < 0)
+            {
+                snprintf(response, sizeof(response), "ERROR %s\n", strerror(errno));
+                send_all(sock, response, strlen(response));
+                continue;
+            }
+            off_t size = lseek(fd, 0, SEEK_END);
+            lseek(fd, 0, SEEK_SET);
+            snprintf(response, sizeof(response), "OK %ld\n", (long)size);
+            send_all(sock, response, strlen(response));
+
+            char buf[FILE_SIZE];
+            ssize_t n;
+            off_t remaining = size;
+            while (remaining > 0)
+            {
+                n = read(fd, buf, sizeof(buf) < remaining ? sizeof(buf) : remaining);
+                if (n <= 0)
+                {
+                    break;
+                }
+                if (send_all(sock, buf, n) < 0)
+                {
+                    break;
+                }
+                remaining -= n;
+            }
+            close(fd);
+        }
+        else
+        {
+            send_all(sock, "UNKNOWN COMMAND\n", 16);
+        }
+    }
+
+    nclients--;
+    printf("-disconnect\n");
+    printusers();
+}
+
+ssize_t read_line(int sock, char *buf, size_t maxlen)
+{
+    size_t i = 0;
+    ssize_t n;
+    char c;
+    while (i < maxlen - 1)
+    {
+        n = recv(sock, &c, 1, 0);
+        if (n <= 0)
+        {
+            return n;
+        }
+        if (c == '\n')
+        {
+            break;
+        }
+        buf[i++] = c;
+    }
+    buf[i] = '\0';
+    return i;
+}
+
+ssize_t send_all(int sock, const void *buf, size_t len)
+{
+    size_t total = 0;
+    const char *ptr = (const char *)buf;
+    while (total < len)
+    {
+        ssize_t n = send(sock, ptr + total, len - total, 0);
+        if (n <= 0)
+        {
+            return n;
+        }
+        total += n;
+    }
+    return total;
 }
